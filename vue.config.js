0151d70a73e3c8496159180b7adb95e20683158b
playDeath <863834859@qq.com>
2022-01-20 17:03:05 +0800

    feat(program init): music_player program init

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  outputDir: './build',
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       views: '@/views'
+  //     }
+  //   }
+  // }
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     views: '@/views'
+  //   }
+  // },
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('views', '@/views')
+      .set('components', '@components')
+      .set('assets', '@assets')
+      .set('router', '@router')
+      .set('store', '@store')
+      .set('network', '@network')
+      .set('utils', '@utils')
+  }
+}
